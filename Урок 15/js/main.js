985e0a655e561697dfdc44386e114de104bd103c
VladaS20 <cssnooo3641@mail.ru>
2025-04-17 20:33:59 +0300

    Update main.js

--- Урок 15/js/main.js
+++ Урок 15/js/main.js
@@ -1,4 +1,6 @@
 (function () {
+  // Бургер
+
   document.addEventListener("click", burgerInit);
 
   function burgerInit(e) {
@@ -14,6 +16,8 @@
       document.body.classList.remove("body--opened-menu");
     }
   }
+
+  // Модалка
 
   const modal = document.querySelector(".modal");
   const modalButton = document.querySelector(".about__img-button");
@@ -47,5 +51,69 @@
     ) {
       body.classList.toggle("body--opened-modal");
     }
+  });
+
+  // Табы
+
+  const tabControls = document.querySelector(".tab-controls");
+
+  tabControls.addEventListener("click", toggleTab);
+
+  function toggleTab(e) {
+    const tabControl = e.target.closest(".tab-controls__link");
+
+    if (!tabControl) return;
+    e.preventDefault();
+    if (tabControl.classList.contains("tab-controls__link--active")) return;
+
+    const tabContentID = tabControl.getAttribute("href");
+    const tabContent = document.querySelector(tabContentID);
+    const activeContent = document.querySelector(".tab-content--show");
+    const activeControl = document.querySelector(".tab-controls__link--active");
+
+    if (activeControl) {
+      activeControl.classList.remove("tab-controls__link--active");
+    }
+    if (activeContent) {
+      activeContent.classList.remove("tab-content--show");
+    }
+
+    tabControl.classList.add("tab-controls__link--active");
+    tabContent.classList.add("tab-content--show");
+  }
+
+  // Аккордеон
+
+  const accordionLists = document.querySelectorAll(".accordion-list");
+
+  accordionLists.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      
+      const accordionList = e.currentTarget;
+      const accordionOpenedItem = accordionList.querySelector(
+        ".accordion-list__item--opened"
+      );
+      const accordionOpenedContent = accordionList.querySelector(
+        ".accordion-list__item--opened .accordion-list__content"
+      );
+
+      const accordionControl = e.target.closest(".accordion-list__control");
+      if (!accordionControl) return;
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+
+      if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+        accordionOpenedItem.classList.remove("accordion-list__item--opened");
+        accordionOpenedContent.style.maxHeight = null;
+      }
+
+      accordionItem.classList.toggle("accordion-list__item--opened");
+
+      if (accordionItem.classList.contains("accordion-list__item--opened")) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
+    });
   });
 })();
